0e76c0ca9fe0fd3e653880d34bee13b34f6c2567
mfairchild365 <mfairchild365@gmail.com>
2013-04-27 16:06:17 -0500

    Display register and login forms

--- www/templates/html/Chat/User/RequiredLoginException.tpl.php
+++ www/templates/html/Chat/User/RequiredLoginException.tpl.php
@@ -1,1 +1,15 @@
-YOU FOOL! YOU NEED TO LOG IN!
+<div class='row-fluid'>
+    <div class="alert">
+        <strong>Warning!</strong> You need to log in to view this site.
+    </div>
+</div>
+
+<div class='row-fluid'>
+    <div class="span6">
+        <?php echo $savvy->render(new \Chat\User\Login()); ?>
+    </div>
+
+    <div class="span6">
+        <?php echo $savvy->render(new \Chat\User\Register()); ?>
+    </div>
+</div>
