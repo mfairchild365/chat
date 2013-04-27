08c1f4ca41699b9554db1fe74c1956bfbdc632b6
mfairchild365 <mfairchild365@gmail.com>
2013-04-27 15:31:43 -0500

    start the session once and add a require login function

--- src/Chat/User/Service.php
+++ src/Chat/User/Service.php
@@ -5,9 +5,11 @@ use Symfony\Component\HttpFoundation\Session\Session;
 
 class Service
 {
+    protected static $session;
+
     public static function logIn(\Chat\User\User $user)
     {
-        $session = new Session();
+        $session = self::getSession();
         $session->start();
 
         $session->set('user.id', $user->id);
@@ -15,15 +17,31 @@ class Service
 
     public static function logOut()
     {
-        $session = new Session();
+        $session = self::getSession();
         $session->clear();
         $session->invalidate();
     }
 
     public static function getCurrentUser()
     {
-        $session = new Session();
+        $session = self::getSession();
 
         return User::getByID($session->get('user.id'));
+    }
+
+    public static function requireLogin()
+    {
+        if (!self::getCurrentUser()) {
+            throw new RequiredLoginException();
+        }
+    }
+
+    public static function getSession()
+    {
+        if (!self::$session) {
+            self::$session = new Session();
+        }
+
+        return self::$session;
     }
 }
