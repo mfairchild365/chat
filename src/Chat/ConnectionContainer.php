521cf555af4f29b9f6a53a0794e19883d32e8740
mfairchild365 <mfairchild365@gmail.com>
2013-04-27 17:43:04 -0500

    add docblock comments and fix vars

--- src/Chat/ConnectionContainer.php
+++ src/Chat/ConnectionContainer.php
@@ -3,29 +3,44 @@ namespace Chat;
 
 class ConnectionContainer
 {
-    protected $connection = false; // \Wrench\Connection object
+    /**
+     * @var bool|\Wrench\Connection $connection
+     */
+    protected $connection = false;
 
     function __construct(\Ratchet\ConnectionInterface $connection)
     {
         $this->setConnection($connection);
     }
 
+    /**
+     * @return bool|\Wrench\Connection
+     */
     public function getConnection()
     {
         return $this->connection;
     }
 
+    /**
+     * @return bool|\Chat\User\User
+     */
     public function getUser()
     {
-        return User\Record::getUser($this->connection, $this->mac);
+        return User\User::getUser($this->connection, $this->mac);
     }
 
+    /**
+     * @param \Ratchet\ConnectionInterface $connection
+     */
     public function setConnection(\Ratchet\ConnectionInterface $connection)
     {
         $this->connection = $connection;
     }
 
-
+    /**
+     * @param $action
+     * @param $data
+     */
     public function send($action, $data)
     {
         Application::sendMessageToClient($this->getConnection(), $action, $data);
