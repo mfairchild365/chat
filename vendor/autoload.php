2aaf66d2014775cee2ea1c13ef0011553082cf61
mfairchild365 <mfairchild365@gmail.com>
2013-04-27 14:44:25 -0500

    add symphony session to composer

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit99596ea4b5c947eac66a5ea1c9a5d495::getLoader();
+return ComposerAutoloaderInitc9f5f14cbff5b0093d1124f5bc56b767::getLoader();
