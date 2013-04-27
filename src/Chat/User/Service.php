d9fe44a96ad4e39af5c803d24400e8f0790a0e1c
mfairchild365 <mfairchild365@gmail.com>
2013-04-27 14:42:51 -0500

    add a user service class

---
+++ src/Chat/User/Service.php
@@ -0,0 +1,28 @@
+<?php
+namespace Chat\User;
+
+use Symfony\Component\HttpFoundation\Session\Session;
+
+class Service
+{
+    public static function logIn(\Chat\User\User $user)
+    {
+        $session = new Session();
+        $session->start();
+
+        $session->set('user.id', $user->id);
+    }
+
+    public static function logOut()
+    {
+        $session = new Session();
+        $session->invalidate();
+    }
+
+    public static function getCurrentUser()
+    {
+        $session = new Session();
+
+        return User::getByID($session->get('user.id'));
+    }
+}
