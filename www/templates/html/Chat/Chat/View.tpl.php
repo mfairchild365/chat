4bca2410967ec11157eb95c9bb5c3b313861c887
mfairchild365 <mfairchild365@gmail.com>
2013-04-27 16:58:39 -0500

    add js to init the cat app

--- www/templates/html/Chat/Chat/View.tpl.php
+++ www/templates/html/Chat/Chat/View.tpl.php
@@ -1,1 +1,7 @@
 <script src="<?php echo \Chat\Config::get('URL');?>www/templates/html/js/chat.js"></script>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        app.init("ws://<?php echo \Chat\Config::get('SERVER_ADDR'); ?>:<?php echo \Chat\Config::get('SERVER_PORT'); ?>/chat", '<?php echo \Chat\Config::get('URL')?>');
+    });
+</script>
