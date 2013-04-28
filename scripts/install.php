1edb72d017936b0792defa730486b3e4ddf9555e
mfairchild365 <mfairchild365@gmail.com>
2013-04-28 18:28:04 -0500

    add an install script

---
+++ scripts/install.php
@@ -0,0 +1,48 @@
+<?php
+require_once dirname(__DIR__) . "/init.php";
+
+function exec_sql($db, $sql, $message, $fail_ok = false)
+{
+    echo $message.PHP_EOL;
+    try {
+        $result = true;
+        if ($db->multi_query($sql)) {
+            do {
+                /* store first result set */
+                if ($result = $db->store_result()) {
+                    $result->free();
+                }
+
+                if (!$db->more_results()) {
+                    break;
+                }
+            } while ($db->next_result());
+        } else {
+            echo "Query Failed: " . $db->error . PHP_EOL;
+        }
+    } catch (Exception $e) {
+        $result = false;
+        if (!$fail_ok) {
+            echo 'The query failed:'.$result->errorInfo();
+            exit();
+        }
+    }
+    echo 'finished.'.PHP_EOL;
+    echo '------------------------------------------'.PHP_EOL;
+    return $result;
+}
+
+$db = \Chat\Util::getDB();
+
+$sql = "";
+
+if (isset($argv[1]) && $argv[1] == '-f') {
+    echo "Deleting old install" . PHP_EOL;
+    $sql .= "SET FOREIGN_KEY_CHECKS=0;
+             DROP TABLE IF EXISTS users;
+             DROP TABLE IF EXISTS messages;
+             SET FOREIGN_KEY_CHECKS=1;";
+}
+$sql .= file_get_contents(dirname(__DIR__) . "/data/database.sql");
+
+exec_sql($db, $sql, 'updatating database');
