d3a5a96abbbfb4394c56a22d6af5d016f6d77652
mfairchild365 <mfairchild365@gmail.com>
2013-04-28 16:34:52 -0500

    remove uneeded divs

--- www/templates/html/Chat/Chat/View.tpl.php
+++ www/templates/html/Chat/Chat/View.tpl.php
@@ -5,3 +5,19 @@
         app.init("ws://<?php echo \Chat\Config::get('SERVER_ADDR'); ?>:<?php echo \Chat\Config::get('SERVER_PORT'); ?>/chat", '<?php echo \Chat\Config::get('URL')?>');
     });
 </script>
+
+<div class='span3 well'>
+    <h2>Users</h2>
+    <ul id='user-list'>
+
+    </ul>
+</div>
+<div class='span8 well'>
+    <div id='chat-container'>
+        <ul id='message-list'>
+
+        </ul>
+        <textarea cols='6' class='span9' id='message' disabled='disabled'></textarea>
+    </div>
+</div>
+</div>
