e8e478555b8257f9648cf23fe6556b7b9da7e69b
mfairchild365 <mfairchild365@gmail.com>
2013-05-05 04:16:48 -0500

    add a basic user profile

--- www/templates/html/Chat/User/View.tpl.php
+++ www/templates/html/Chat/User/View.tpl.php
@@ -1,1 +1,13 @@
-USER PROFILE
+<h2><?php echo $context->username; ?></h2>
+
+<?php
+echo $savvy->render($context, '/Chat/User/SubNav.tpl.php');
+?>
+
+<ul>
+    <li>First Name: <?php echo $context->first_name ?></li>
+    <li>Last Name: <?php echo $context->last_name ?></li>
+    <li>Site Role: <?php echo $context->role ?></li>
+    <li>Site Status: <?php echo $context->status ?></li>
+    <li>Chat Status: <?php echo $context->chat_status ?></li>
+</ul>
