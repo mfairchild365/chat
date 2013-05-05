69e0d1b4a8750a78adfbaa4592636370d4c3adc6
mfairchild365 <mfairchild365@gmail.com>
2013-05-05 04:16:09 -0500

    add a user edit template

---
+++ www/templates/html/Chat/User/Edit.tpl.php
@@ -0,0 +1,54 @@
+<h2><?php echo $context->username; ?></h2>
+
+<?php
+echo $savvy->render($context, '/Chat/User/SubNav.tpl.php');
+?>
+
+<script language='javascript' type='text/javascript'>
+    function verifyPasswordCheck(input) {
+        if (input.value != document.getElementById('input-password').value) {
+            input.setCustomValidity('The two passwords must match.');
+        } else {
+            // input is valid -- reset the error message
+            input.setCustomValidity('');
+        }
+    }
+</script>
+
+<form action="<?php echo \Chat\Config::get('URL')?>" method="post" class="form-horizontal">
+    <div class="control-group">
+        <label class="control-label" for="input-firstname">First Name</label>
+        <div class="controls">
+            <input type="text" id="input-firstname" name="first_name" placeholder="First Name" value="<?php $context->first_name?>" />
+        </div>
+    </div>
+    <div class="control-group">
+        <label class="control-label" for="input-lastname">Last Name</label>
+        <div class="controls">
+            <input type="text" id="input-lastname" name="last_name" placeholder="Last Name" value="<?php $context->first_name?>" />
+        </div>
+    </div>
+    <div class="control-group">
+        <label class="control-label" for="input-email">Email</label>
+        <div class="controls">
+            <input type="email" id="input-email" name="email" required placeholder="Email" value="<?php $context->email?>" />
+        </div>
+    </div>
+    <div class="control-group">
+        <label class="control-label" for="input-password">Password</label>
+        <div class="controls">
+            <input type="password" id="input-password" name="password" placeholder="Leave blank to keep current" />
+        </div>
+    </div>
+    <div class="control-group">
+        <label class="control-label" for="input-password-verify">Verify Password</label>
+        <div class="controls">
+            <input type="password" id="input-password-verify" name="password_verify" placeholder="Leave blank to keep current" oninput="verifyPasswordCheck(this)" />
+        </div>
+    </div>
+    <div class="control-group">
+        <div class="controls">
+            <button type="submit" class="btn">Update</button>
+        </div>
+    </div>
+</form>
