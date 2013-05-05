1ba35f55a2254107b690e9e42cea818a8ab464ee
mfairchild365 <mfairchild365@gmail.com>
2013-05-05 04:16:34 -0500

    add a sub nav template for users

---
+++ www/templates/html/Chat/User/SubNav.tpl.php
@@ -0,0 +1,10 @@
+<div class='subnav'>
+    <ul class="nav nav-pills">
+        <li class="active">
+            <a href="<?php echo $context->getURL();?>">Profile</a>
+        </li>
+        <li>
+            <a href="<?php echo $context->getURL();?>/edit">Edit</a>
+        </li>
+    </ul>
+</div>
