f399d9f0c46f5e56be05de27e71e74516951e270
mfairchild365 <mfairchild365@gmail.com>
2013-05-06 18:57:02 -0500

    readability

--- src/Chat/Setting/Edit.php
+++ src/Chat/Setting/Edit.php
@@ -32,6 +32,9 @@ class Edit extends Setting implements PostHandlerInterface
             $setting->save();
         }
 
-        \Chat\Controller::redirect(\Chat\Config::get('URL') . "admin", new \Chat\FlashBagMessage("success", "Settings have been saved"));
+        \Chat\Controller::redirect(
+            \Chat\Config::get('URL') . "admin",
+            new \Chat\FlashBagMessage("success", "Settings have been saved")
+        );
     }
 }
