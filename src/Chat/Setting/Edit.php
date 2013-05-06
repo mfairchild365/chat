90338093cdc2bbe46366ab29002b757f183896f2
mfairchild365 <mfairchild365@gmail.com>
2013-05-06 18:28:34 -0500

    add a view for editing settings

--- src/Chat/Setting/Edit.php
+++ src/Chat/Setting/Edit.php
@@ -7,11 +7,31 @@ class Edit extends Setting implements PostHandlerInterface
 {
     function __construct($options = array())
     {
+        if (!$user = \Chat\User\Service::getCurrentUser()) {
+            throw new \Chat\User\RequiredLoginException();
+        }
 
+        if ($user->role != 'ADMIN') {
+            throw new \Chat\User\NotAuthorizedException();
+        }
     }
 
     function handlePost($get, $post, $files)
     {
-        print_r($post); exit();
+        if (!isset($post['settings'])) {
+            throw new \Exception("Settings array does not exist", 400);
+        }
+
+        foreach ($post['settings'] as $id=>$value)
+        {
+            if (!$setting = Setting::getByID($id)) {
+                throw new \Exception("Could not find setting with ID: $id", 400);
+            }
+
+            $setting->setting_value = $value;
+            $setting->save();
+        }
+
+        \Chat\Controller::redirect(\Chat\Config::get('URL') . "admin");
     }
 }
---
+++ www/templates/html/Chat/Setting/Edit.tpl.php
@@ -0,0 +1,23 @@
+<h2>Settings</h2>
+
+
+<form action="<?php echo \Chat\Config::get('URL')?>admin" method="post" class="form-horizontal">
+    <?php
+    foreach (\Chat\Setting\Settings::getAllSettings() as $setting)
+    {
+        ?>
+        <div class="control-group">
+        <label class="control-label" for="input-<?php echo $setting->setting_name; ?>"><?php echo $setting->setting_name; ?></label>
+        <div class="controls">
+            <input type="text" id="input-firstname" name="settings[<?php echo $setting->id; ?>]" placeholder="<?php echo $setting->setting_name; ?>" value="<?php echo $setting->setting_value; ?>" />
+        </div>
+        </div>
+        <?php
+    }
+    ?>
+    <div class="control-group">
+        <div class="controls">
+            <button type="submit" class="btn">Update</button>
+        </div>
+    </div>
+</form>
