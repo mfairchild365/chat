e50618a38023c330e34ffdfe5cb31f3d0044a9e3
mfairchild365 <mfairchild365@gmail.com>
2013-05-05 20:02:29 -0500

    add a settings table

--- data/database.sql
+++ data/database.sql
@@ -2,6 +2,8 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
+CREATE SCHEMA IF NOT EXISTS `DEFAULTDATABASENAME` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
+USE `DEFAULTDATABASENAME` ;
 
 -- -----------------------------------------------------
 -- Table `DEFAULTDATABASENAME`.`users`
@@ -43,6 +45,18 @@ CREATE  TABLE IF NOT EXISTS `DEFAULTDATABASENAME`.`messages` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 PACK_KEYS = DEFAULT;
+
+
+-- -----------------------------------------------------
+-- Table `DEFAULTDATABASENAME`.`Settings`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `DEFAULTDATABASENAME`.`Settings` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `setting_name` VARCHAR(256) NOT NULL ,
+  `setting_value` VARCHAR(256) NULL ,
+  PRIMARY KEY (`id`) ,
+  UNIQUE INDEX `key_UNIQUE` (`setting_name` ASC) )
+ENGINE = InnoDB;
 
 
 
