0c7235403d13fbb0974d77a4c7df466618cdac85
mfairchild365 <mfairchild365@gmail.com>
2013-05-08 18:11:10 -0500

    echo current input values

--- www/templates/html/Chat/User/Edit.tpl.php
+++ www/templates/html/Chat/User/Edit.tpl.php
@@ -19,19 +19,19 @@ echo $savvy->render($context, '/Chat/User/SubNav.tpl.php');
     <div class="control-group">
         <label class="control-label" for="input-firstname">First Name</label>
         <div class="controls">
-            <input type="text" id="input-firstname" name="first_name" placeholder="First Name" value="<?php $context->first_name?>" />
+            <input type="text" id="input-firstname" name="first_name" placeholder="First Name" value="<?php echo $context->first_name?>" />
         </div>
     </div>
     <div class="control-group">
         <label class="control-label" for="input-lastname">Last Name</label>
         <div class="controls">
-            <input type="text" id="input-lastname" name="last_name" placeholder="Last Name" value="<?php $context->first_name?>" />
+            <input type="text" id="input-lastname" name="last_name" placeholder="Last Name" value="<?php echo $context->first_name?>" />
         </div>
     </div>
     <div class="control-group">
         <label class="control-label" for="input-email">Email</label>
         <div class="controls">
-            <input type="email" id="input-email" name="email" required placeholder="Email" value="<?php $context->email?>" />
+            <input type="email" id="input-email" name="email" required placeholder="Email" value="<?php echo $context->email?>" />
         </div>
     </div>
     <div class="control-group">
