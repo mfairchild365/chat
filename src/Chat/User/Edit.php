747167f73d7fdd1bc0ef78d13f396ca99ef8f817
mfairchild365 <mfairchild365@gmail.com>
2013-05-08 18:11:47 -0500

    check authorization when editing a user

--- src/Chat/User/Edit.php
+++ src/Chat/User/Edit.php
@@ -10,6 +10,14 @@ class Edit extends User implements PostHandlerInterface
         if (isset($options['id']) && $object = self::getByID($options['id'])) {
             $this->synchronizeWithArray($object->toArray());
         }
+
+        if (!$user = Service::getCurrentUser()) {
+            throw new RequiredLoginException();
+        }
+
+        if ($user->role != 'ADMIN' && $user->id != $this->id) {
+            throw new NotAuthorizedException();
+        }
     }
 
     function handlePost($get, $post, $files)
