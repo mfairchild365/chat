f096f753c90a7038ffcfcadf3852fd330a90c274
mfairchild365 <mfairchild365@gmail.com>
2013-05-08 18:51:41 -0500

    require the editurl in the post interface

--- src/Chat/PostHandlerInterface.php
+++ src/Chat/PostHandlerInterface.php
@@ -4,4 +4,11 @@ namespace Chat;
 interface PostHandlerInterface
 {
     public function handlePost($get, $post, $files);
+
+    /*
+     * Should return an absolute url for this view.
+     *
+     * @return string url
+     */
+    public function getEditURL();
 }
