b045b857fe95f8a0e60a868dea7d2bec05cea82b
mfairchild365 <mfairchild365@gmail.com>
2013-05-11 14:52:31 -0500

    Update the plugin to use the viewable interface

--- plugins/home/src/View.php
+++ plugins/home/src/View.php
@@ -1,7 +1,15 @@
 <?php
 namespace Chat\Plugins\Home;
 
-class View
+class View implements \Chat\ViewableInterface
 {
+    public function getPageTitle()
+    {
+        return "Home";
+    }
 
+    public function getURL()
+    {
+        return \Chat\Config::get('URL') . 'home';
+    }
 }
