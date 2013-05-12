1c9cba24abce7f637a8d6e15197ae06c378c4bea
mfairchild365 <mfairchild365@gmail.com>
2013-05-11 22:12:13 -0500

    add a the starts of a gravatar plugin

---
+++ plugins/gravatar/src/Initialize.php
@@ -0,0 +1,35 @@
+<?php
+namespace Chat\Plugins\Gravatar;
+
+class Initialize
+{
+    public $options = array();
+
+    public function __construct($options = array())
+    {
+
+    }
+
+    public function initialize()
+    {
+
+    }
+
+    public function getEventListeners()
+    {
+        $listeners = array();
+
+        $listeners[] = array(
+            'event'    => \Chat\Events\JavascriptCompile::EVENT_NAME,
+            'listener' => function (\Chat\Events\JavascriptCompile $event) {
+                if (get_class($event->getView()) == 'Chat\Chat\View') {
+                    $event->addScript(\Chat\Config::get('URL') . 'plugins/gravatar/www/templates/html/js/main.js');
+                }
+
+            }
+        );
+
+        return $listeners;
+    }
+
+}
