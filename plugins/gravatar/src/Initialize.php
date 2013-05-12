22d70a19feb07a460b4f5c596a39d26cfdfc1046
mfairchild365 <mfairchild365@gmail.com>
2013-05-12 12:57:58 -0500

    rename the gravatar js to be more descriptive

--- plugins/gravatar/src/Initialize.php
+++ plugins/gravatar/src/Initialize.php
@@ -23,7 +23,7 @@ class Initialize
             'event'    => \Chat\Events\JavascriptCompile::EVENT_NAME,
             'listener' => function (\Chat\Events\JavascriptCompile $event) {
                 if (get_class($event->getView()) == 'Chat\Chat\View') {
-                    $event->addScript(\Chat\Config::get('URL') . 'plugins/gravatar/www/templates/html/js/main.js');
+                    $event->addScript(\Chat\Config::get('URL') . 'plugins/gravatar/www/templates/html/js/gravatar.js');
                     $event->addScript(\Chat\Config::get('URL') . 'plugins/gravatar/www/templates/html/js/md5-min.js');
                 }
 
