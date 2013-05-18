1d802b6a376ba3bb7d5e25767b31f26d81c5c109
mfairchild365 <mfairchild365@gmail.com>
2013-05-18 17:15:35 -0500

    add headers to the two options

--- www/templates/html/Chat/User/RequiredLoginException.tpl.php
+++ www/templates/html/Chat/User/RequiredLoginException.tpl.php
@@ -6,10 +6,12 @@
 
 <div class='row-fluid'>
     <div class="span6">
+        <h3>Login</h3>
         <?php echo $savvy->render(new \Chat\User\Login()); ?>
     </div>
 
     <div class="span6">
+        <h3>Or Register</h3>
         <?php echo $savvy->render(new \Chat\User\Register()); ?>
     </div>
 </div>
