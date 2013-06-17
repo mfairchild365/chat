89f5daa1e3230746634f704ca5ab840750ba880a
mfairchild365 <mfairchild365@gmail.com>
2013-06-17 18:50:25 -0500

    Initialize the plugins plugin as a plugin
    :p

---
+++ src/Chat/Plugin/Initialize.php
@@ -0,0 +1,20 @@
+<?php
+namespace Chat\Plugin;
+
+class Initialize implements \Chat\Plugin\InitializePluginInterface
+{
+    public function __construct(array $options)
+    {
+
+    }
+
+    public function initialize()
+    {
+
+    }
+
+    public function getEventListeners()
+    {
+        return array();
+    }
+}
