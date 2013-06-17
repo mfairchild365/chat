3a90bfe84523e006f4ad7531b32f882d0f3f4d31
mfairchild365 <mfairchild365@gmail.com>
2013-06-17 18:12:21 -0500

    Add a plugin Interface

---
+++ src/Chat/PluginInterface.php
@@ -0,0 +1,19 @@
+<?php
+namespace Chat;
+
+interface PluginInterface
+{
+    /**
+     * Called when a plugin is installed.  Add sql changes and other logic here.
+     *
+     * @return mixed
+     */
+    public function onInstall();
+
+    /**
+     * Please undo whatever you did in onInstall().  If you don't, someone might have a bad day.
+     *
+     * @return mixed
+     */
+    public function onUnInstall();
+}
