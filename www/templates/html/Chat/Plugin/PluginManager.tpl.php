a67d5fe53b8593d993ab9103b98d458805a23fbd
mfairchild365 <mfairchild365@gmail.com>
2013-06-17 21:00:36 -0500

    pre-populate check boxes if already installed

--- www/templates/html/Chat/Plugin/PluginManager.tpl.php
+++ www/templates/html/Chat/Plugin/PluginManager.tpl.php
@@ -2,6 +2,10 @@
     <?php
     foreach (\Chat\Plugin\PluginManager::getExternalPlugins() as $name=>$plugin)
     {
+        $checked = "";
+        if ($plugin->isInstalled()) {
+            $checked = 'checked="checked"';
+        }
         ?>
         <div class="controls">
             <h3><?php echo $plugin->getName(); ?></h3>
@@ -10,7 +14,7 @@
             </p>
 
             <label class="checkbox">
-                <input type="checkbox" name="enabled_plugins[]" value="<?php echo $name ?>"> Enable
+                <input type="checkbox" name="enabled_plugins[]" value="<?php echo $name ?>" <?php echo $checked ?>> Enable
             </label>
 
         </div>
@@ -19,7 +23,7 @@
     ?>
     <div class="control-group">
         <div class="controls">
-            <button type="submit" class="btn">Update</button>
+            <button type="submit" class="btn" name='update'>Update</button>
         </div>
     </div>
 </form>
