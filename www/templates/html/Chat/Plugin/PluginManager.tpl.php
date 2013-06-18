e95ce1c10a5ff6efc7d8a3bb3e277e474bec009f
mfairchild365 <mfairchild365@gmail.com>
2013-06-17 19:32:48 -0500

    add a form for the plugin manager

--- www/templates/html/Chat/Plugin/PluginManager.tpl.php
+++ www/templates/html/Chat/Plugin/PluginManager.tpl.php
@@ -1,1 +1,25 @@
-plugin manager
+<form action="<?php echo $context->getEditURL()?>" method="post" class="form-horizontal">
+    <?php
+    foreach (\Chat\Plugin\PluginManager::getExternalPlugins() as $name=>$plugin)
+    {
+        ?>
+        <div class="controls">
+            <h3><?php echo $plugin->getName(); ?></h3>
+            <p>
+                <?php echo $plugin->getDescription() ?>
+            </p>
+
+            <label class="checkbox">
+                <input type="checkbox" name="enabled_plugins[]" value="<?php echo $name ?>"> Enable
+            </label>
+
+        </div>
+        <?php
+    }
+    ?>
+    <div class="control-group">
+        <div class="controls">
+            <button type="submit" class="btn">Update</button>
+        </div>
+    </div>
+</form>
