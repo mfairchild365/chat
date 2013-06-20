6f04654dd6407754d28b6109d66665f5e1483302
mfairchild365 <mfairchild365@gmail.com>
2013-06-19 19:50:10 -0500

    add the steam id to the user object

--- plugins/steam/src/Initialize.php
+++ plugins/steam/src/Initialize.php
@@ -19,6 +19,23 @@ class Initialize implements \Chat\Plugin\InitializePluginInterface
     {
         $listeners = array();
 
+        $listeners[] = array(
+
+            'event'    => \Chat\DB\Events\Record\AlterFields::EVENT_NAME,
+            'listener' => function (\Chat\DB\Events\Record\AlterFields $event) {
+                $record = $event->getRecord();
+
+                if ($record->getTable() != 'users') {
+                    return;
+                }
+
+                //Add the steam_id_64 field
+                $fields = $event->getFields();
+                $fields['steam_id_64'] = null;
+                $event->setFields($fields);
+            }
+        );
+
         return $listeners;
     }
 
