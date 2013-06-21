12af7d746511d625dada87dd1f13843657843eb7
mfairchild365 <mfairchild365@gmail.com>
2013-06-20 19:25:14 -0500

    Add the steam API key as a setting

--- plugins/steam/src/Plugin.php
+++ plugins/steam/src/Plugin.php
@@ -5,28 +5,34 @@ class Plugin extends \Chat\Plugin\PluginInterface
 {
     public function onInstall()
     {
+        $db = \Chat\Util::getDB();
+
+        //Add the steam_id_64 col to the users table.
         $sql = 'ALTER TABLE users
                 ADD steam_id_64 VARCHAR(256)';
 
-        $db = \Chat\Util::getDB();
         $db->query($sql);
 
-        /**
-         * TODO: add hooks to db class, so that $user->steam_id_64 is accessible.
-         * Hook would be something like DB::getFields() and append the steam_id_64 field.
-         * It won't show up as a property in the code, but it should work like this.
-         */
+        //Add the STEAM_API_KEY row to the settings table
+        $setting = new \Chat\Setting\Setting();
+        $setting->setting_name = 'STEAM_API_KEY';
+        $setting->save();
 
         return true;
     }
 
     public function onUninstall()
     {
+        $db = \Chat\Util::getDB();
+
         $sql = 'ALTER TABLE users
                 DROP COLUMN steam_id_64';
 
-        $db = \Chat\Util::getDB();
         $db->query($sql);
+
+        //remove the STEAM_API_KEY row
+        $setting = \Chat\Setting\Setting::getBySettingName('STEAM_API_KEY');
+        $setting->delete();
 
         return true;
     }
