89a7f1e80658e393d653de9b87bc47c70ed72336
mfairchild365 <mfairchild365@gmail.com>
2013-06-23 17:45:39 -0500

    Add a function to get steam user info

--- plugins/steam/src/Service.php
+++ plugins/steam/src/Service.php
@@ -13,4 +13,27 @@ class Service
 
         return self::$api;
     }
+
+    public static function getSteamUserInfo()
+    {
+        //Get steam stats for all users.
+        $map = array();
+
+        foreach(\Chat\User\RecordList::getAll() as $user) {
+            //Skip users that haven't connected to steam yet.
+            if (!$user->steam_id_64) {
+                continue;
+            }
+
+            $map[$user->id] = $user->steam_id_64;
+        }
+
+        $steamUsers = Service::getAPI()->getUsers(array_values($map));
+
+        foreach ($steamUsers as $key=>$steamUser) {
+            $steamUsers[$key]->users_id = array_search($steamUser->steamid, $map);
+        }
+
+        return $steamUsers;
+    }
 }
